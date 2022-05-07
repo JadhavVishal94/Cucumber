702829dd687b22a6095adad4e488843d9d4386fc
Vishal <Vishal@LAPTOP-35V3QI65>
2022-05-07 16:18:15 +0530

    new class created

---
+++ src/main/java/com/BaseLayer/BaseHrm.java
@@ -0,0 +1,12 @@
+package com.BaseLayer;
+
+public class BaseHrm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("new project created");
+
+	}
+
+}
